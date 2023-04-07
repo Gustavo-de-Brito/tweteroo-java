913f244f911b4475af7ebe731abb4453be098dab
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2023-04-07 20:21:22 -0300

    feat: throw an error if username is already registered

--- src/main/java/com/tweteroo/api/repositories/AuthRepository.java
+++ src/main/java/com/tweteroo/api/repositories/AuthRepository.java
@@ -1,9 +1,12 @@
 package com.tweteroo.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tweteroo.api.models.User;
 
 public interface AuthRepository extends JpaRepository<User, Long> {
-  
+
+  List<User> findByUsername(String username);
 }
