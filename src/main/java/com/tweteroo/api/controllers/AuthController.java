e1050add5a752efe70fbbcddc8a487e56473f5b1
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2023-04-07 19:55:12 -0300

    feat: add new user to the database

--- src/main/java/com/tweteroo/api/controllers/AuthController.java
+++ src/main/java/com/tweteroo/api/controllers/AuthController.java
@@ -1,18 +1,23 @@
 package com.tweteroo.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dto.UserDTO;
+import com.tweteroo.api.models.User;
+import com.tweteroo.api.services.AuthService;
 
 @RestController
 @RequestMapping("/sign-up")
 public class AuthController {
+  @Autowired
+  private AuthService service;
 
   @PostMapping
   public void create(@RequestBody UserDTO req) {
-
+    this.service.create(new User(req));
   }
 }
